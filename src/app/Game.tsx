4483e0239dd73e490c09732245b46c5cdfe4a2a9
erobert <etiennerobert33@gmail.com>
2023-07-08 00:06:24 +0200

    Add Bob

--- src/app/Game.tsx
+++ src/app/Game.tsx
@@ -6,6 +6,7 @@ function Game() {
   return (
     <div className={styles.root}>
       <Character name="Alice" position={{ x: 100, y: 100 }} />
+      <Character name="Bob" position={{ x: 10, y: 20 }} />
     </div>
   );
 }
