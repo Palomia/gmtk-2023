5ff85ac82a4fbf5228ed8d1f67aacd6f7e2d65cf
Amaury Palomino <amaury.palomino@outlook.com>
2023-07-07 23:28:48 +0200

    Create ActionBank

---
+++ src/app/ActionBank.tsx
@@ -0,0 +1,12 @@
+function ActionBank() {
+  return (
+    <ul>
+      <li>Move</li>
+      <li>Attack</li>
+      <li>Defense</li>
+      <li>fake Attack</li>
+    </ul>
+  );
+}
+
+export default ActionBank;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import Title from './Title';
+import ActionBank from './ActionBank';
 
 export default function Home() {
-  return <Title text="Hello Folks" />;
+  return <ActionBank />;
 }
