debdb963f68e27aba8562c2c84a4b0067ce1f166
Amaury Palomino <amaury.palomino@outlook.com>
2023-07-07 23:48:33 +0200

    Mutable and colored ActionBank

--- src/app/ActionBank.tsx
+++ src/app/ActionBank.tsx
@@ -1,12 +1,20 @@
+import stringToColour from './stringToColour';
+
+const listActions = [
+  'Move',
+  'Attack',
+  'Defense',
+  'Fake Attack',
+  'Heavy Attack',
+];
+const htmlActions = listActions.map((action) => (
+  <li key={action} style={{ backgroundColor: stringToColour(action) }}>
+    {action}
+  </li>
+));
+
 function ActionBank() {
-  return (
-    <ul>
-      <li>Move</li>
-      <li>Attack</li>
-      <li>Defense</li>
-      <li>fake Attack</li>
-    </ul>
-  );
+  return <ul>{htmlActions}</ul>;
 }
 
 export default ActionBank;
