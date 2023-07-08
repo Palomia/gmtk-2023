4c3efaa243be507842543f595410f72feb8cf5d5
Amaury Palomino <amaury.palomino@outlook.com>
2023-07-08 13:44:49 +0200

    Create Editor.tsx

---
+++ src/app/Editor.tsx
@@ -0,0 +1,28 @@
+'use client';
+import { useState } from 'react';
+import ActionBank from './ActionBank';
+import ActionSequence from './ActionSequence';
+
+function Editor() {
+  //state
+  const [sequence, setSequence] = useState([{ name: 'Move' }]);
+  //comportement
+  const deleteAction = (index: number) => {
+    // @ts-expect-error Typing is not up to date
+    setSequence((sequence) => sequence.toSpliced(index, 1));
+  };
+
+  const addAction = (name: string) => {
+    setSequence((sequence) => [...sequence, { name }]);
+  };
+
+  //render
+  return (
+    <div>
+      <ActionSequence deleteAction={deleteAction} sequence={sequence} />{' '}
+      <ActionBank addAction={addAction} />
+    </div>
+  );
+}
+
+export default Editor;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,6 @@
-'use client';
-import { useState } from 'react';
-import ActionBank from './ActionBank';
-import ActionSequence from './ActionSequence';
+import Editor from './Editor';
 
 export default function Home() {
-  //state
-  const [sequence, setSequence] = useState([{ name: 'Move' }]);
-  //comportement
-  const deleteAction = (index: number) => {
-    // @ts-expect-error Typing is not up to date
-    setSequence((sequence) => sequence.toSpliced(index, 1));
-  };
-
-  const addAction = (name: string) => {
-    setSequence((sequence) => [...sequence, { name }]);
-  };
-
   //render
-  return (
-    <div>
-      <ActionSequence deleteAction={deleteAction} sequence={sequence} />{' '}
-      <ActionBank addAction={addAction} />
-    </div>
-  );
+  return <Editor />;
 }
