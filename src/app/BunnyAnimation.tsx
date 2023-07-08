daf9fdd487f8b1b59db7c6898bd78181ff5e66b6
erobert <etiennerobert33@gmail.com>
2023-07-08 02:50:56 +0200

    Create BunnyAnimation component

---
+++ src/app/BunnyAnimation.tsx
@@ -0,0 +1,58 @@
+import Animation from './Animation';
+import spritesheet from '../Sprout Lands - Sprites - premium pack/characters/Premium Charakter Spritesheet.png';
+import type { Direction } from './types';
+
+type AnimationType =
+  | 'idle'
+  | 'walk'
+  | 'run'
+  | 'shovel'
+  | 'axe'
+  | 'watering can';
+
+function animationNumber(animationType: AnimationType) {
+  switch (animationType) {
+    case 'idle':
+      return 0;
+    case 'walk':
+      return 1;
+    case 'run':
+      return 2;
+    case 'shovel':
+      return 3;
+    case 'axe':
+      return 4;
+    case 'watering can':
+      return 5;
+  }
+}
+
+function directionNumber(direction: Direction) {
+  switch (direction) {
+    case 'down':
+      return 0;
+    case 'up':
+      return 1;
+    case 'left':
+      return 2;
+    case 'right':
+      return 3;
+  }
+}
+
+type Props = {
+  direction: Direction;
+  animationType: AnimationType;
+};
+
+export default function BunnyAnimation({ animationType, direction }: Props) {
+  return (
+    <Animation
+      file={spritesheet.src}
+      width={48}
+      height={48}
+      frames={8}
+      line={animationNumber(animationType) * 4 + directionNumber(direction)}
+    />
+  );
+}
--- src/app/Character.tsx
+++ src/app/Character.tsx
@@ -1,39 +1,20 @@
 import style from './Character.module.css';
-import Animation from './Animation';
-import spritesheet from '../Sprout Lands - Sprites - premium pack/characters/Premium Charakter Spritesheet.png';
+import BunnyAnimation from './BunnyAnimation';
+import type { Direction } from './types';
 
 type Props = {
   name: string;
-  direction?: 'up' | 'down' | 'left' | 'right';
+  direction?: Direction;
   position: {
     x: number;
     y: number;
   };
 };
 
-function getAnimationLine(direction: Props['direction']) {
-  switch (direction) {
-    case 'down':
-      return 0;
-    case 'up':
-      return 1;
-    case 'left':
-      return 2;
-    case 'right':
-      return 3;
-  }
-}
-
 function Character({ name, position: { x, y }, direction = 'down' }: Props) {
   return (
     <h1 className={style.root} style={{ left: `${x}px`, top: `${y}px` }}>
-      <Animation
-        file={spritesheet.src}
-        width={48}
-        height={48}
-        frames={8}
-        line={getAnimationLine(direction)}
-      />
+      <BunnyAnimation animationType="watering can" direction={direction} />
     </h1>
   );
 }
